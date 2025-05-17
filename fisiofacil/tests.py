f941b0b78e9259e06fbebc609cd9fc408cd0a770
Alexandre <alexandre.lourenco@outlook.com>
2025-05-16 21:47:33 -0300

    teste

--- fisiofacil/tests.py
+++ fisiofacil/tests.py
@@ -1,6 +1,12 @@
-import unittest
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-class HelloWordTestCase(unittest.TestCase):
-    
-    def test_hello_world(self):
-        self.assertEqual(1, 1)
+class SimpleViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_superuser(username='admin', password='admin123', email='admin@test.com')
+        self.client.login(username='admin', password='admin123')
+
+    def test_index_adm_status_code(self):
+        response = self.client.get('/adm/')
+        self.assertEqual(response.status_code, 200)
